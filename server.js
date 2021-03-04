9af60fa30518a183fa204218be991d3455837833
Ben <pauleybj@gmail.com>
2021-03-04 16:44:43 +0000

    added get routes for css, js and notes db

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const path = require("path");
 const uniqid = require("uniqid");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/", "index.html"));
 });
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/", "notes.html"));
 });
-
-const PORT = process.env.PORT || 5000;
+app.get("/assets/css/styles.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/assets/css/", "styles.css"));
+});
+app.get("/assets/js/index.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/assets/js/", "index.js"));
+});
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/", "db.json"));
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
