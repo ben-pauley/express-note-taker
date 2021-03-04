ac271b038e21c99ef15c57cc329fc75ba35b6593
Ben <pauleybj@gmail.com>
2021-03-04 15:22:19 +0000

    added html routes and fixed paths in html files

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const path = require("path");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/", "index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/", "notes.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
