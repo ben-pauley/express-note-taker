a2e0bf7d5e1a11be5eb4f431a2b46d9cbdef3953
Ben <pauleybj@gmail.com>
2021-03-04 18:11:39 +0000

    added static route and modified get routes

--- server.js
+++ server.js
@@ -4,26 +4,30 @@ const path = require("path");
 const uniqid = require("uniqid");
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/", "index.html"));
-});
+app.use("/", express.static("./public"));
+
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/", "notes.html"));
 });
-app.get("/assets/css/styles.css", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/assets/css/", "styles.css"));
-});
-app.get("/assets/js/index.js", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/assets/js/", "index.js"));
-});
+
 app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./db/", "db.json"));
+  fs.readFile("./db/db.json", function (err, data) {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post("/api/notes", (req, res) => {});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/", "index.html"));
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
